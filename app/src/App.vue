29c6b13a8567cbe60086c6951996f45a949a109e
turtlethumbs <alexander.r.shepherd@protonmail.com>
2024-11-02 00:04:59 +0700

    chore: rename HelloWorld component to TheTaskList component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <TheTaskList msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import TheTaskList from './components/TheTaskList.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    TheTaskList
   }
 });
 </script>
