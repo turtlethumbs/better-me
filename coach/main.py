41d87e630267c803621fe591befbfb393a05881e
turtlethumbs <alexander.r.shepherd@protonmail.com>
2024-11-02 01:57:54 +0700

    feat: use ollama to get chat bot to respond

--- coach/main.py
+++ coach/main.py
@@ -1,10 +1,37 @@
+import requests
 import json
+import os
+from dotenv import load_dotenv
+from urllib.parse import urljoin
+
+load_dotenv()
+
+def send_input_to_ollama(prompt):
+    url = urljoin(os.getenv("OLLAMA_API_URL"), "generate")
+    payload = {
+        "prompt": prompt,
+        "model": "llama3.2"
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        output = ""
+        lines = response.text.split("\n")
+        for line in lines:
+            json_obj = json.loads(line or '{}')
+            output += json_obj.get('response') or ""
+        return output
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
 
 def main():
     with open("../api/data/tasks.json", 'r') as json_file:
         tasks = json.load(json_file)
-    
-    
 
+    for task in tasks:
+        if not task['completed']:
+            result = send_input_to_ollama('I did not complete task ' + task['title'])
+            print(result)
+    
 if __name__ == "__main__":
     main()
